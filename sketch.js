d48229e6c2c81eec8c2002d79ecbc39f78167651
NeilMani602 <72112430+NeilMani602@users.noreply.github.com>
2020-10-08 19:45:56 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var fixedRect, movingRect;
+function setup() {
+  createCanvas(1200,800);
+ fixedRect =  createSprite(400, 200, 50, 80);
+ fixedRect.shapeColor = "green";
+fixedRect.debug = true;
+ movingRect =  createSprite(400, 200, 80, 30);
+ movingRect.shapeColor = "green";
+ movingRect.debug = true;
+ movingRect.velocityY= -5;
+ fixedRect.velocityY = 5;
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  if( movingRect.x-fixedRect.x< fixedRect.width/2+ movingRect.width/2
+    && fixedRect.x-movingRect.x< fixedRect.width/2+ movingRect.width/2
+    ){
+      movingRect.velocityX=movingRect.velocityX *(-1);
+      fixedRect.velocityX=fixedRect.velocityX *(-1);
+  }
+  drawSprites();
+}
+
